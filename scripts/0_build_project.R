ef964f4c83b5ad111783a33d5364e9296b979c99
Languillaume, Antoine <antoine.languillaume@wur.nl>
2020-05-06 11:57:40 +0200

    add comments in 0_build_script.R

--- scripts/0_build_project.R
+++ scripts/0_build_project.R
@@ -1,4 +1,8 @@
+##### Text Analyis: The Black Adder
+#### Antoine Languillaume
+#### Build Script
 
+# Install required packages if necessary
 required_packages <- c("tidyverse", "tidytext", "magick", "here", "knitr", "rmarkdown")
 
 for (package in required_packages) {
@@ -7,10 +11,13 @@ for (package in required_packages) {
 	}
 }
 
+
+# Run all scripts in the correct order
 all_scripts <- list.files("scripts", full.names = TRUE) 
 
-target_scripts <- all_scripts[grep("[1-9]", all_scripts)]
-
+target_scripts <- all_scripts[grep("[1-9]", all_scripts)
+															]
 for(script in target_scripts) source(script, local = TRUE)
 
+# Build the final report
 rmarkdown::render("report/The_Cunning_Report.Rmd")
