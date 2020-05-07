e030372b4c6acf491c869751c1530a7f23b5d350
Languillaume, Antoine <antoine.languillaume@wur.nl>
2020-05-07 13:12:30 +0200

    correct build project

--- scripts/0_build_project.R
+++ scripts/0_build_project.R
@@ -3,20 +3,20 @@
 #### Build Script
 
 # Install required packages if necessary
-required_packages <- c("tidyverse", "tidytext", "magick", "here", "knitr", "rmarkdown")
+required_packages <- c("tidyverse", "tidytext", "ggpubr", 
+											 "magick", "here", "knitr", "rmarkdown")
 
 for (package in required_packages) {
-	if("xtable" %in% rownames(installed.packages()) == FALSE){
+	if(package %in% rownames(installed.packages()) == FALSE){
 		install.packages(package, dependencies = TRUE)
 	}
 }
 
-
 # Run all scripts in the correct order
 all_scripts <- list.files("scripts", full.names = TRUE) 
 
-target_scripts <- all_scripts[grep("[1-9]", all_scripts)
-															]
+target_scripts <- all_scripts[grep("[1-9]", all_scripts)]
+
 for(script in target_scripts) source(script, local = TRUE)
 
 # Build the final report
