0d59aa7b492720c3dd71b9b311c456aaabc13342
Languillaume, Antoine <antoine.languillaume@wur.nl>
2020-06-16 15:15:28 +0200

    Add binder files

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse")
+install.packages("tidytext")
+install.packages("ggpubr")
+install.packages("magick")
+install.packages("here")
+install.packages("knitr")
+install.packages("rmarkdown")
